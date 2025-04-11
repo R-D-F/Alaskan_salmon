5619383467384430277b8d03b8864491e7dee95a
riley <rifrancis22@gmail.com>
2025-04-10 21:30:50 -0700

    created and started working on the arc script script.py

--- scratch.py
+++ scratch.py
@@ -1,72 +1,74 @@
 import json
 import rapidfuzz
 
-file = r"data\json\combined.json"
+print("this is working too")
 
-json_streams = []
-with open(file, "r") as f:
-    data = json.load(f)["DATA"]
-    for i in data:
-        if i[5] in json_streams:
-            pass
-        else:
-            json_streams.append(i[5])
+# file = r"data\json\combined.json"
 
-to_fuzz = []
-stream_list = r"data\json\streams_list.json"
+# json_streams = []
+# with open(file, "r") as f:
+#     data = json.load(f)["DATA"]
+#     for i in data:
+#         if i[5] in json_streams:
+#             pass
+#         else:
+#             json_streams.append(i[5])
 
-# print(f"gdb data {json_streams}")
-gdb_streams = []
-with open(stream_list, "r") as f:
-    gdb_streams = json.load(f)
-    for i in json_streams:
-        if i not in gdb_streams:
-            to_fuzz.append(i)
+# to_fuzz = []
+# stream_list = r"data\json\streams_list.json"
 
-# no_match_dict = {}
-# for i in to_fuzz:
-#     best_match = rapidfuzz.process.extractOne(
-#         i, gdb_streams, scorer=rapidfuzz.fuzz.ratio
-#     )
+# # print(f"gdb data {json_streams}")
+# gdb_streams = []
+# with open(stream_list, "r") as f:
+#     gdb_streams = json.load(f)
+#     for i in json_streams:
+#         if i not in gdb_streams:
+#             to_fuzz.append(i)
 
-#     no_match_dict[i] = best_match[0]
-#     print(f"From JSON {i} {best_match}")
-#     print("\n")
-#     print(no_match_dict)
+# # no_match_dict = {}
+# # for i in to_fuzz:
+# #     best_match = rapidfuzz.process.extractOne(
+# #         i, gdb_streams, scorer=rapidfuzz.fuzz.ratio
+# #     )
 
-x = {
-    "Afognak River (Litnik)": "Afognak River",
-    "Copper River (Miles L)": "Copper River",
-    "Deshka": "Deshka River / Kroto Creek",
-    "Jim Creek": "Jims Creek",
-    "Kasilof River (sockeye)": "Kasilof River",
-    "Kenai River (Chinook)": "Kenai River",
-    "Kenai River (late-run sockeye)": "Kandik River",
-    "Klag": None,
-    "Little Susitna": "Little Susitna River",
-    "McLees Lake": None,
-    "Nelson River (Sapsuk)": "Necons River",
-    "Olga Creek (Upper Station)": "Olga Creek",
-    "Orzinski Lake": None,
-    "Pasagshak": "*Pasagshak River",
-    "Pauls Bay River": None,
-    "Redoubt Lake": None,
-}
+# #     no_match_dict[i] = best_match[0]
+# #     print(f"From JSON {i} {best_match}")
+# #     print("\n")
+# #     print(no_match_dict)
 
-with open(file, "r") as f:
-    data = json.load(f)
+# x = {
+#     "Afognak River (Litnik)": "Afognak River",
+#     "Copper River (Miles L)": "Copper River",
+#     "Deshka": "Deshka River / Kroto Creek",
+#     "Jim Creek": "Jims Creek",
+#     "Kasilof River (sockeye)": "Kasilof River",
+#     "Kenai River (Chinook)": "Kenai River",
+#     "Kenai River (late-run sockeye)": "Kandik River",
+#     "Klag": None,
+#     "Little Susitna": "Little Susitna River",
+#     "McLees Lake": None,
+#     "Nelson River (Sapsuk)": "Necons River",
+#     "Olga Creek (Upper Station)": "Olga Creek",
+#     "Orzinski Lake": None,
+#     "Pasagshak": "*Pasagshak River",
+#     "Pauls Bay River": None,
+#     "Redoubt Lake": None,
+# }
 
-# Add new column
-new_column = "GDB_COUNTLOCATION"
-data["COLUMNS"].append(new_column)
+# with open(file, "r") as f:
+#     data = json.load(f)
 
-# Add default value to each data row
-for row in data["DATA"]:
-    if row[5] in gdb_streams:
-        row.append(row[5])
-    else:
-        row.append(x[row[5]])
+# # Add new column
+# new_column = "GDB_COUNTLOCATION"
+# data["COLUMNS"].append(new_column)
 
-# Save it back
-with open(file, "w") as f:
-    json.dump(data, f, indent=4)
+# # Add default value to each data row
+# for row in data["DATA"]:
+#     if row[5] in gdb_streams:
+#         row.append(row[5])
+#     else:
+#         row.append(x[row[5]])
+
+# # Save it back
+# with open(file, "w") as f:
+#     json.dump(data, f, indent=4)
---
+++ tool_box/script.py
@@ -0,0 +1,41 @@
+import arcpy
+import json
+import os
+
+json_path = r"..\data\json\combined.json"
+fc = os.path.abspath(r"..\arc_pro\alaskan_salmon\AWC2024.gdb\AWC\AWC_stream")
+fields = ["OID@", "SHAPE@", "NAME"]
+
+# Read JSON data
+with open(json_path, "r") as f:
+    data = json.load(f)
+
+# Output location
+gdb = os.path.abspath(r"..\arc_pro\alaskan_salmon\alaskan_salmon.gdb")
+output_fc = os.path.join(gdb, "json_table_fc")
+
+
+# Extract fields from JSON
+fields = list(data["COLUMNS"])
+rows = data["DATA"]
+
+arcpy.CreateTable_management(gdb, "json_table_fc")
+
+# Add fields
+# TODO detect type:
+for field in fields:
+    arcpy.AddField_management(
+        output_fc, field, "TEXT"
+    )  # You can detect type if needed
+
+# Insert rows
+with arcpy.da.InsertCursor(output_fc, fields) as cursor:
+    for row in rows:
+        # TODO convert based on type. add location
+        str_row = [str(val) for val in row]  # convert all to string
+        cursor.insertRow(str_row)
+
+# with arcpy.da.SearchCursor(fc, fields) as cursor:
+#     for row in cursor:
+#         print(row[1])
+#         break
