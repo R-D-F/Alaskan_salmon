26be7b25b2b7d06794572db0c120b2898dc02a68
riley <rifrancis22@gmail.com>
2025-05-18 14:56:57 -0700

    scratch work

--- scratch.py
+++ scratch.py
@@ -1,74 +1,10 @@
 import json
-import rapidfuzz
 
-print("this is working too")
+with open("data\json\combined.json", "r") as f:
+    data = json.load(f)
 
-# file = r"data\json\combined.json"
-
-# json_streams = []
-# with open(file, "r") as f:
-#     data = json.load(f)["DATA"]
-#     for i in data:
-#         if i[5] in json_streams:
-#             pass
-#         else:
-#             json_streams.append(i[5])
-
-# to_fuzz = []
-# stream_list = r"data\json\streams_list.json"
-
-# # print(f"gdb data {json_streams}")
-# gdb_streams = []
-# with open(stream_list, "r") as f:
-#     gdb_streams = json.load(f)
-#     for i in json_streams:
-#         if i not in gdb_streams:
-#             to_fuzz.append(i)
-
-# # no_match_dict = {}
-# # for i in to_fuzz:
-# #     best_match = rapidfuzz.process.extractOne(
-# #         i, gdb_streams, scorer=rapidfuzz.fuzz.ratio
-# #     )
-
-# #     no_match_dict[i] = best_match[0]
-# #     print(f"From JSON {i} {best_match}")
-# #     print("\n")
-# #     print(no_match_dict)
-
-# x = {
-#     "Afognak River (Litnik)": "Afognak River",
-#     "Copper River (Miles L)": "Copper River",
-#     "Deshka": "Deshka River / Kroto Creek",
-#     "Jim Creek": "Jims Creek",
-#     "Kasilof River (sockeye)": "Kasilof River",
-#     "Kenai River (Chinook)": "Kenai River",
-#     "Kenai River (late-run sockeye)": "Kandik River",
-#     "Klag": None,
-#     "Little Susitna": "Little Susitna River",
-#     "McLees Lake": None,
-#     "Nelson River (Sapsuk)": "Necons River",
-#     "Olga Creek (Upper Station)": "Olga Creek",
-#     "Orzinski Lake": None,
-#     "Pasagshak": "*Pasagshak River",
-#     "Pauls Bay River": None,
-#     "Redoubt Lake": None,
-# }
-
-# with open(file, "r") as f:
-#     data = json.load(f)
-
-# # Add new column
-# new_column = "GDB_COUNTLOCATION"
-# data["COLUMNS"].append(new_column)
-
-# # Add default value to each data row
-# for row in data["DATA"]:
-#     if row[5] in gdb_streams:
-#         row.append(row[5])
-#     else:
-#         row.append(x[row[5]])
-
-# # Save it back
-# with open(file, "w") as f:
-#     json.dump(data, f, indent=4)
+rivers = []
+for i in data["DATA"]:
+    if i[7] not in rivers:
+        rivers.append(i[7])
+print(rivers)
--- tool_box/script.py
+++ tool_box/script.py
@@ -1,37 +1,14 @@
 import arcpy
 import json
 import os
-from datetime import datetime
-
-fc_path = r"C:\Users\rifra\Homework\Alaskan_salmon\arc_pro\alaskan_salmon\alaskan_salmon.gdb\json_table_fc"
-
-if arcpy.Exists(fc_path):
-    arcpy.Delete_management(fc_path)
 
 json_path = r"..\data\json\combined.json"
 fc = os.path.abspath(r"..\arc_pro\alaskan_salmon\AWC2024.gdb\AWC\AWC_stream")
 fields = ["OID@", "SHAPE@", "NAME"]
-name_shape = {}
-
-with arcpy.da.SearchCursor(fc, fields) as cursor:
-    for row in cursor:
-        name_shape[row[2]] = row[1]
-
-field_type_dict = {
-    "YEAR": "SHORT",
-    "COUNTDATE": "DATE",
-    "FISHCOUNT": "LONG",
-    "SPECIESID": "SHORT",
-    "COUNTLOCATIONID": "SHORT",
-    "COUNTLOCATION": "TEXT",
-    "SPECIES": "TEXT",
-    "GDB_COUNTLOCATION": "TEXT",
-    "SHAPE@": "POINT",
-}
 
 # Read JSON data
 with open(json_path, "r") as f:
-    data = json.load(f)
+    json_data = json.load(f)
 
 # Output location
 gdb = os.path.abspath(r"..\arc_pro\alaskan_salmon\alaskan_salmon.gdb")
@@ -39,59 +16,24 @@ output_fc = os.path.join(gdb, "json_table_fc")
 
 
 # Extract fields from JSON
-fields = list(data["COLUMNS"])
-fields.append("SHAPE@")
-rows = data["DATA"]
+fields = list(json_data["COLUMNS"])
+rows = json_data["DATA"]
 
-arcpy.CreateFeatureclass_management(
-    gdb, "json_table_fc", geometry_type="POLYLINE", spatial_reference=fc
-)
+arcpy.CreateTable_management(gdb, "json_table_fc")
 
 # Add fields
 # TODO detect type:
 for field in fields:
-    if field == "SHAPE@":
-        continue  # SHAPE@ is implicit, don't try to add it manually
     arcpy.AddField_management(
-        output_fc, field, field_type_dict[field]
+        output_fc, field, "TEXT"
     )  # You can detect type if needed
 
-
 # Insert rows
-def convert_value(value, field_type):
-    if value in (None, "", " "):
-        return None
-
-    elif field_type == "TEXT":
-        return str(value)
-    elif field_type in ("SHORT", "LONG"):
-        return int(value)
-    elif field_type in ("FLOAT", "DOUBLE"):
-        return float(value)
-    elif field_type == "DATE":
-
-        return datetime.strptime(
-            str(value), "%B, %d %Y %H:%M:%S"
-        )  # or whatever format you're using
-    elif field_type == "POINT":
-        return value
-    else:
-        return value  # fallback
-
-
 with arcpy.da.InsertCursor(output_fc, fields) as cursor:
     for row in rows:
-        gdb_location = row[
-            -1
-        ]  # Assuming 'GDB_COUNTLOCATION' is the last column
-        shape = name_shape.get(gdb_location, None) if gdb_location else None
-        row.append(shape)  # Add geometry to row
-        converted_row = [
-            convert_value(val, field_type_dict[field])
-            for val, field in zip(row, fields)
-        ]
-        cursor.insertRow(converted_row)
-
+        # TODO convert based on type. add location
+        str_row = [str(val) for val in row]  # convert all to string
+        cursor.insertRow(str_row)
 
 # with arcpy.da.SearchCursor(fc, fields) as cursor:
 #     for row in cursor:
