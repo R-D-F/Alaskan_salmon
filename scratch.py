95b1659e519bcd31c6604470a5c6feecf710729b
riley <rifrancis22@gmail.com>
2025-05-18 22:59:19 -0700

    arc'ed around

--- scratch.py
+++ scratch.py
@@ -1,10 +1,16 @@
-import json
+import pandas as pd
 
-with open("data\json\combined.json", "r") as f:
+import json
+import pandas as pd
+import openpyxl
+
+# Load the JSON file
+with open(r"data\json\combinedV2.json") as f:
     data = json.load(f)
 
 rivers = []
 for i in data["DATA"]:
     if i[7] not in rivers:
         rivers.append(i[7])
+
 print(rivers)
